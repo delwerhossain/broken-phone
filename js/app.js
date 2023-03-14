d19b307ca4cb5c174f9a9cd5e191d7afc8f94395
delwerHossain <delwerhossain006@gmail.com>
2023-03-14 10:50:24 +0000

    apple

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const loadPhones = async(searchText, dataLimit) =>{
 
     const res = await fetch(url);
     const data = await res.json();
-    console.log(dataLimit);
+    console.log(data.data.length);
     displayPhones(data.data, dataLimit);
 }
 
@@ -31,12 +31,12 @@ const displayPhones = (phones, dataLimit) => {
     }
     // display all phones
     phones.forEach(phone => {
-        // console.log(phone.slug);
+        console.log(phone);
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.images}" class="card-img-top" alt="...">
+            <img src="${phone.image1}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -107,5 +107,6 @@ const displayPhoneDetails = phone =>{
         <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
     `
 }
+
 
 loadPhones('apple');
